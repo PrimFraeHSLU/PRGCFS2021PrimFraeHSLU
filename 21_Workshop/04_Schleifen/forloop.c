daa7afe35877c12b426c828c0cc1b4de9a27fda3
Frank <bla@bla.com>
2021-03-18 20:33:15 +0100

    added new files

---
+++ 21_Workshop/04_Schleifen/forloop.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+int main() {
+
+	// Aufgabe 1
+	int sum = 0;
+	int start = 1;
+	int end = 10;
+	
+	for( int i = start; i <= end; i++ )
+	{
+		sum = sum + i;
+	}
+	
+	printf("The sum from %d to %d is equal to %d\n", start, end, sum);
+	printf("\n");
+
+	// Aufgabe 2
+	int sum1 = 0;
+	int start1 = 2;
+	int end1 = 10;
+	
+	for( int i1 = start1; i1 <= end1; i1 = i1 + 2 )
+	{
+		sum1 = sum1 + i1;
+	}
+	
+	printf("The sum from %d to %d is equal to %d\n", start1, end1, sum1);
+	printf("\n");
+	printf("****************************************************************\n");
+
+	//Aufgabe 3
+	int start2 = 40;
+	int end2 = 25;
+	
+	for( int i2 = start2; i2 >= end2; i2 = i2-1 )
+	{
+		printf(" %d\n", i2);
+	}
+	
+	printf("****************************************************************\n");
+
+
+	//Aufgabe 4
+	int start3 = 1;
+	int end3 = 800;
+	
+	for( int i3 = start3; i3 <= end3; i3 = i3*3 )
+	{
+		printf(" %d\n", i3);
+	}
+	
+	printf("****************************************************************\n");
+
+
+	//Aufgabe 5
+	int start4 = 0;
+	int end4 = 10;
+	
+	for( int i4 = start4; i4 < end4; i4++ )
+	{
+		for( int y = start4 + 1; y <= end; y++)
+		{
+		printf(" %d", i4*10+y);
+		}
+		printf("\n");
+	}
+	
+	printf("****************************************************************\n");
+
+
+
+	
+	
+	
+	
+	
+	return 0;
+}
+
+
--- 40_Excercices/04_CLA_Operators/logicOperators.c
+++ 40_Excercices/04_CLA_Operators/logicOperators.c
@@ -1,21 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+const int NUM_VALUES = 3;
 
 int main(int argc, char *argv[] )
 {
 	//falls NICHT 3 Argumente erhalten, dann if Schleife
-	if( argc != 3 ){
+	if( argc != NUM_VALUES + 1 ){
 		printf("Usage: \n");
-		printf("logicOperators Input1 Input2 \n");
+		printf("logicOperators Input1 Input2 Input3 \n");
 	}
 	else
 	{	
 		// genau 3 argumente erhalten
+		int val1 = atoi(argv[1]);
+		int val2 = atoi(argv[2]);
+		int val3 = atoi(argv[3]);
+		int sum = 0;
+		float average = 0.0f;
+		
+		printf("%d, %d, %d\n", val1, val2, val3);
+		
+		if( !(val1 > 0) || !(val2 > 0) || !(val3 > 0) )
+		{
+			printf("All inputs must be stricly greater than zero.\n");
+		}
+		else
+		{
+			if( val1 >= val2 && val1 >= val3 )
+			{
+				printf("%d is the greatest\n", val1);
+			}
+			else if( val2 >= val1 && val2 >= val3 )
+			{
+				printf("%d is the greatest\n", val2);
+			}
+			else
+			{
+				printf("%d is the greatest\n", val3);
+			}
+			
+			if( val1 <= val2 && val1 <= val3 )
+			{
+				printf("%d is the smallest\n", val1);
+			}
+			else if( val2 <= val1 && val2 <= val3 )
+			{
+				printf("%d is the smallest\n", val2);
+			}
+			else
+			{
+				printf("%d is the smallest\n", val3);
+			}
+			
+			printf("inputs valid\n");
+			
+			sum = val1 + val2 + val3;
+			printf("The sum is: %d\n", sum );
+			
+			average = (float)sum/NUM_VALUES;
+			printf("The average is: %.2f\n", average );
+		}	
+	}
+		
+#if 0		
+		if(
+		
 		printf("Number of input arguments: %d \n", argc ); //anzahl args. printen
 
 		for( int i = 0; i < argc; i++ )
 		{
 			printf("Argument %d was %s \n", i , argv[i]);
 		}
-	}		
+	}
+#endif		
 	return 0;
 }
