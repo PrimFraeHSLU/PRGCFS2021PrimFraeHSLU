a84aa8549465ea0d7f56fe770718c2ba2e24ccdc
Frank <bla@bla.com>
2021-05-15 18:05:01 +0200

    csv test

---
+++ 90_AusgabeTestat/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void binaer (long i)
+{
+	if(i<2)
+		printf("%li", i);
+	else
+	{
+		binaer (i/2);
+		printf ("%li" , i%2);
+	}
+}
+
+int main(void)
+{
+	long dezimal = 7;
+	binaer (dezimal);
+	return 0;
+}
